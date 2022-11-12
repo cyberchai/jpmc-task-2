046cd90d057634b16a8870b8ca35a240d27a8154
cyberchai <chairachananharder@gmail.com>
2022-11-12 14:50:40 -0500

    moving on for now

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import DataStreamer, { ServerRespond } from './DataStreamer';
 import Graph from './Graph';
 import './App.css';
+import {setInterval} from "timers";
 
 /**
  * State declaration for <App />
@@ -39,19 +40,35 @@ class App extends Component<{}, IState> {
   /**
    * Get new data from server and update the state with the new data
    */
+  // getDataFromServer() {
+  //   let x = 0;
+  //   const interval = setInterval(() => {
+  //     DataStreamer.getData((serverResponds: ServerRespond[]) => {
+  //       // Update the state by creating a new array of data that consists of
+  //       // Previous data in the state and the new data from server
+  //       this.setState({
+  //         data: serverResponds,
+  //         showGraph: true,
+  //       });
+  //     });
+  //     x++;
+  //     if(x > 1000){
+  //       clearInterval(interval);
+  //     }
+  //   }, 100);
+  // }
+
   getDataFromServer() {
     let x = 0;
     const interval = setInterval(() => {
       DataStreamer.getData((serverResponds: ServerRespond[]) => {
-        // Update the state by creating a new array of data that consists of
-        // Previous data in the state and the new data from server
         this.setState({
           data: serverResponds,
-          showGraph: true,
+          showGraph: true
         });
       });
       x++;
-      if(x > 1000){
+      if (x > 1000) {
         clearInterval(interval);
       }
     }, 100);
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -30,16 +30,39 @@ class Graph extends Component<IProps, {}> {
     return React.createElement('perspective-viewer');
   }
 
+  // componentDidMount() {
+  //   // Get element to attach the table from the DOM.
+  //   // const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+  //   const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+  //
+  //   elem.setAttribute('view', 'yline');
+  //   elem.setAttribute('column-pivots', '["stock"]');
+  //   elem.setAttribute('row-pivots', '["timestamp"]');
+  //   elem.setAttribute('columns', 'top_ask_price');
+  //   elem.setAttribute('aggregates', '{"stock":"distinct count", "top ask price":"avg", "top bid price" : "avg", "timestamp": "distinct count"}');
+  //
+  //
+  //   const schema = {
+  //     stock: 'string',
+  //     top_ask_price: 'float',
+  //     top_bid_price: 'float',
+  //     timestamp: 'date',
+  //   };
+  //
+  //   if (window.perspective && window.perspective.worker()) {
+  //     this.table = window.perspective.worker().table(schema);
+  //   }
+  //   if (this.table) {
+  //     // Load the `table` in the `<perspective-viewer>` DOM reference.
+  //
+  //     // Add more Perspective configurations here.
+  //     elem.load(this.table);
+  //   }
+  // }
+
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
-
-    elem.setAttribute('view', 'yline');
-    elem.setAttribute('column-pivots', '["stock"]');
-    elem.setAttribute('row-pivots', '["timestamp"]');
-    elem.setAttribute('columns', 'top_ask_price');
-    elem.setAttribute('aggregates', '{"stock":"distinct count", "top ask price":"avg", "top bid price" : "avg", "timestamp": "distinct count"}');
-
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
@@ -56,6 +79,13 @@ class Graph extends Component<IProps, {}> {
 
       // Add more Perspective configurations here.
       elem.load(this.table);
+      elem.setAttribute("view", "y_line");
+      elem.setAttribute("column-pivots", '["stock"]');
+      elem.setAttribute("row_pivots", '["timestamp"]');
+      elem.setAttribute("columns", '["top_ask_price"]');
+      elem.setAttribute("aggregates",
+          '{"stock":"distinct_count", "top_ask_price":"avg", "top_bid_price":"avg", "timestamp":"distinct_count"}'
+      );
     }
   }
 
